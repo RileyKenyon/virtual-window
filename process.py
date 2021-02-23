87bfe03ee6dcf69e77db4e2d8c459483008467a1
RileyKenyon <rike2277@colorado.edu>
2021-02-22 21:10:02 -0700

    Added deadband on position and flipped y direction

--- process.py
+++ process.py
@@ -22,7 +22,7 @@ if p is None:
 detections = []
 
 class VirtualWindow:
-    def __init__(self,src,width,height,camera_on=False):
+    def __init__(self,src,width,height,camera_on=False,annotate=False):
         self.src = src
         self.width = width
         self.height = height
@@ -30,19 +30,24 @@ class VirtualWindow:
         self.window_height = 1080
         self.x_nom = 0.5
         self.y_nom = 0.5
-        self.z_nom = 1
+        self.z_nom = 0
         self.src_width = None
         self.src_height = None
         self.x = 0
         self.y = 0
-        self.z = 1        #m
+        self.z = 0        #m
         self.x0 = None
         self.y0 = None
         self.max_depth = 2
-        self.tau = 0.12
-        self.tau_d = 0.25
+        self.depth_deadband = 0.02
+        self.tau = 0
+        self.tau_d = 0
         self.t = time.time()
+        self.frame_decimate = 60            # number of frames
+        self.frame_count = 0
+        self.deadband = 0                  # px deadband on position
         self.camera_on = camera_on
+        self.annotate = annotate
         
 
     def stream(self):
@@ -51,7 +56,6 @@ class VirtualWindow:
             ret, frame = self.cap.read()
             self.src_width = np.shape(frame)[1]
             self.src_height = np.shape(frame)[0]
-        print(self.src_width,self.src_height)
 
         while True:
             self.get_oak_coord()
@@ -87,7 +91,7 @@ class VirtualWindow:
                     y = int((detection.y_min + detection.y_max)* img_h/2)
                     z = detection.depth_z
                     cv2.rectangle(frame,pt1,pt2,(0,0,255),2)
-                    label = "face ({0},{1},{2:0.2f})".format(x,y,detection.depth_z)
+                    label = "face ({0},{1},{2:0.2f})".format(x,y,z)
                     cv2.putText(frame,label,(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
                 
                 if self.camera_on:
@@ -96,31 +100,45 @@ class VirtualWindow:
                         break
         self.x = x
         self.y = y
-        self.z = z
+        if z is not None:
+            if abs(self.z - z) > self.depth_deadband:
+                self.z = z
 
     def play_video(self):
         window = cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('frame',self.window_width,self.window_height)
+        frame_start = time.time()
         ret, frame = self.cap.read()
         if ret:
             frame = self.crop(frame)
+            frame_end = time.time()
+            label = "fps: {0}".format(1/(frame_end - frame_start))
+            if self.annotate:
+                cv2.putText(frame,label,(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
             cv2.imshow('frame',frame)
         else:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     def crop(self,frame):
         # TODO make dynamic based off window size of oak + depth max
+        new_frame = frame 
         if self.x is not None and self.y is not None and self.z is not None:
-            self.x_nom = self.x / 300                            # normalize
-            self.y_nom = self.y / 300
-        
-        self.width = int(self.z_nom * self.src_width)
-        self.height = int(self.z_nom * self.src_height)
-        x0 = int(self.x_nom * (self.src_width - self.width))     # pixel location 
-        y0 = int(self.y_nom * (self.src_height - self.height))
-        self.lowpass(x0,y0)
-        print(self.z_nom)
-        new_frame = frame[self.y0:self.y0+self.height, self.x0:self.x0+self.width]
+            if (self.frame_count % self.frame_decimate) == 0:
+                self.frame_count = 0
+                x_nom = self.x / 300 # normalize
+                y_nom = self.y /300
+
+                if (abs(self.x_nom - x_nom) > self.deadband/300) or (abs(self.y_nom - y_nom) > self.deadband/300):
+                    self.x_nom = x_nom            
+                    self.y_nom = y_nom
+                    self.width = int(self.z_nom * self.src_width)
+                    self.height = int(self.z_nom * self.src_height)
+                    x0 = int(self.x_nom * (self.src_width - self.width))     # pixel location 
+                    y0 = int((1 - self.y_nom) * (self.src_height - self.height))
+                    self.lowpass(x0,y0)
+
+            if (self.width > 0) and (self.height > 0):
+                new_frame = frame[self.y0:self.y0+self.height-1, self.x0:self.x0+self.width-1]
         return new_frame
     
     def lowpass(self,x,y):
@@ -139,7 +157,7 @@ class VirtualWindow:
             self.y0 = y
 
 if __name__ == "__main__":
-    vw = VirtualWindow('resources\AdobeStock_267854166_Video_HD_Preview.mov', 1280, 720,camera_on=False)
+    vw = VirtualWindow('resources\AdobeStock_267854166_Video_4k_Preview.mov', 1280, 720,camera_on=True,annotate=False)
     vw.stream()
 
 cv2.destroyAllWindows()
